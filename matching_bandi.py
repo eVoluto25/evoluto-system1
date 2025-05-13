88340e33339cd32b38c1c09848c5f9d52293d281
eVoluto25 <info@capitaleaziendale.it>
2025-05-13 10:21:11 +0200

    Update matching_bandi.py

--- matching_bandi.py
+++ matching_bandi.py
@@ -6,7 +6,7 @@ def carica_bandi(csv_path):
 def filtra_bandi_compatibili(bandi_df, caratteristiche_impresa):
     compatibili = []
     for _, bando in bandi_df.iterrows():
-        if (caratteristiche_impresa["forma_agevolazione"] in bando["forma_agevolazione"]
+        if (caratteristiche_impresa["Forma_agevolazione"] in bando["Forma_agevolazione"]
             and caratteristiche_impresa["territorio"] in bando["territorio"]
             and caratteristiche_impresa["beneficiari"] in bando["beneficiari"]
             and caratteristiche_impresa["finalita"] in bando["finalita"]):
