6571859f8b6371a9991054688e5502e4f0b2fb13
eVoluto25 <info@capitaleaziendale.it>
2025-05-16 20:54:27 +0200

    Update and rename aggiornamento-bandi to bandi_utils.py

---
+++ bandi_utils.py
@@ -0,0 +1,9 @@
+# bandi_utils.py
+
+from aggiorna_bandi import file_aggiornato, scarica_file, DATASET_URL, ETAG_FILE, DATASET_FILE
+
+def formatta_bandi():
+    if file_aggiornato(DATASET_URL, ETAG_FILE):
+        scarica_file(DATASET_URL, DATASET_FILE)
+    else:
+        print("⏭️ Nessun aggiornamento disponibile. Dataset invariato.")
