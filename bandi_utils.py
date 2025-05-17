3aa9332fc09f32b16954c3a76f0b7c6366d89ad9
eVoluto25 <info@capitaleaziendale.it>
2025-05-17 06:23:35 +0200

    Update bandi_utils.py

--- bandi_utils.py
+++ bandi_utils.py
@@ -1,6 +1,6 @@
 from aggiorna_bandi import file_aggiornato, scarica_file, DATASET_URL, ETAG_FILE, DATASET_FILE
 
-def formatta_bandi():
+def aggiorna_bandi():
     if file_aggiornato(DATASET_URL, ETAG_FILE):
         scarica_file(DATASET_URL, DATASET_FILE)
     else:
